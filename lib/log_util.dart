908b144eb27351ed28137b1538924dcaf2b9f044
angusaya <52888571@qq.com>
2022-08-18 14:49:17 +0800

    33

--- lib/log_util.dart
+++ lib/log_util.dart
@@ -3,7 +3,7 @@ import 'log_util_platform_interface.dart';
 
 class LogUtil {
   Future<String?> getPlatformVersion() {
+    print('hello');
     return LogUtilPlatform.instance.getPlatformVersion();
   }
-
 }
